f5d175c95530d34dd866a9d0df5639bfd4e820f8
yosef-ZzZ <yosef.zebene1@gmail.com>
2023-08-06 19:55:02 -0500

    created middleware test files

--- tests/controllers/auth.test.js
+++ tests/controllers/auth.test.js
@@ -74,8 +74,3 @@ describe("POST /auth", () => {
         })
     })
 })
-
-// TODO: Test auth middleware by checking if
-//          - Access is denied when missing token
-//          - Requests are successful with valid token
-//          - Access is denied when token is expired
---
+++ tests/middleware/authMiddleware.test.js
@@ -0,0 +1,4 @@
+// TODO: Test auth middleware by checking if
+//          - Access is denied when missing token
+//          - Requests are successful with valid token
+//          - Access is denied when token is expired
